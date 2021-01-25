f38ea2729f10f65f1bc93662379f5e9907f9f321
ita27rmp100 <68165849+ita27rmp100@users.noreply.github.com>
2021-01-25 17:03:07 +0100

    Create tag_functions.py

---
+++ tag_functions.py
@@ -0,0 +1,47 @@
+class html_tag:
+    def DOCTYPE_html ():
+        return "<! DOCTYPE html> \ n"
+    def html (* args):
+        x = ""
+        for i in args: x + = str (i) + "\ n"
+        return f "<html> \ n {x} \ r </html>"
+    def comment (* args):
+        x = ""
+        for i in args: x + = str (i) + "\ n"
+        return f "<! - {x} \ r ->"
+    def tag (name, properties = "", * args):
+        x = ""
+        for i in range (len (args)): x + = args [i] + "\ n"
+        return f "<{name} {properties}> \ n {x} \ r </ {name}>"
+class head_tag:
+        def head (* args):
+            x = ""
+            for i in range (len (args)): x + = args [i] + "\ n"
+            return f "<head> \ n {x} \ r </head>"
+        def title (title):
+            return f "" "<title> {title} </title>" ""
+        def meta (properties = "", * args):
+            x = ""
+            for i in range (len (args)): x + = args [i] + "\ n"
+            return f "" <meta {properties} "> {x} </meta>" ""
+        def link (properties = "", * args):
+            x = ""
+            for i in range (len (args)): x + = args [i] + "\ n"
+            return f "" "<link {properties}"> {x} </link> "" "
+        def style (* args):
+            x = ""
+            for i in range (len (args)): x + = args [i] + "\ n"
+            return f "<style> \ n {x} \ r </style>"
+class body_tag:
+    def body (properties = "", * args):
+        x = ""
+        for i in range (len (args)): x + = args [i] + "\ n"
+        return f "" "<body {properties}"> \ n {x} \ r </body> "" "
+    def div (properties = "", * args):
+        x = ""
+        for i in range (len (args)): x + = args [i] + "\ n"
+        return f "" "<div {properties}"> \ n {x} \ r </div> "" "
+    def pre (properties = "", * args):
+        x = ""
+        for i in range (len (args)): x + = args [i] + "\ n"
+        return f "" <pre {properties} "> \ n {x} \ r </pre>" ""
