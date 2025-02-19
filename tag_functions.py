fb400543ab6917cf0c32448e5401614def243026
Abderahim <ita27rmp100@gmail.com>
2025-02-19 16:51:28 +0100

    fix syntax errors in tag_functions.py

--- tag_functions.py
+++ tag_functions.py
@@ -3,38 +3,38 @@ class html_tag:
     def DOCTYPE_html ():
         return "<! DOCTYPE html> \ n"
     def html (* args):
-        for i in args: x + = str (i) + "\ n"
-        return f "<html> \ n {x} \ r </html>"
+        for i in args: x += str (i) + "\ n"
+        return f"<html> \ n {x} \ r </html>"
     def comment (* args):
-        for i in args: x + = str (i) + "\ n"
-        return f "<! - {x} \ r ->"
+        for i in args: x += str (i) + "\ n"
+        return f"<! - {x} \ r ->"
     def tag (name, properties = "", * args):
-        for i in range (len (args)): x + = args [i] + "\ n"
-        return f "<{name} {properties}> \ n {x} \ r </ {name}>"
+        for i in range (len (args)): x += args [i] + "\ n"
+        return f"<{name} {properties}> \ n {x} \ r </ {name}>"
 class head_tag:
         x = ""
         def head (* args):
-            for i in range (len (args)): x + = args [i] + "\ n"
-            return f "<head> \ n {x} \ r </head>"
+            for i in range (len (args)): x += args [i] + "\ n"
+            return f"<head> \ n {x} \ r </head>"
         def title (title):
-            return f "" "<title> {title} </title>" ""
+            return f"""<title> {title} </title>"""
         def meta (properties = "", * args):
-            for i in range (len (args)): x + = args [i] + "\ n"
-            return f "" <meta {properties} "> {x} </meta>" ""
+            for i in range (len (args)): x += args [i] + "\ n"
+            return f""" <meta {properties} "> {x} </meta>"""
         def link (properties = "", * args):
-            for i in range (len (args)): x + = args [i] + "\ n"
-            return f "" "<link {properties}"> {x} </link> "" "
+            for i in range (len (args)): x += args [i] + "\ n"
+            return f"""<link {properties}"> {x} </link> """
         def style (* args):
-            for i in range (len (args)): x + = args [i] + "\ n"
-            return f "<style> \ n {x} \ r </style>"
+            for i in range (len (args)): x += args [i] + "\ n"
+            return f"<style> \ n {x} \ r </style>"
 class body_tag:
     x = ""
     def body (properties = "", * args):
-        for i in range (len (args)): x + = args [i] + "\ n"
-        return f "" "<body {properties}"> \ n {x} \ r </body> "" "
+        for i in range (len (args)): x += args [i] + "\ n"
+        return f"""<body {properties}"> \ n {x} \ r </body> """
     def div (properties = "", * args):
-        for i in range (len (args)): x + = args [i] + "\ n"
-        return f "" "<div {properties}"> \ n {x} \ r </div> "" "
+        for i in range (len (args)): x += args [i] + "\ n"
+        return f"""<div {properties}"> \ n {x} \ r </div> """
     def pre (properties = "", * args):
-        for i in range (len (args)): x + = args [i] + "\ n"
-        return f "" <pre {properties} "> \ n {x} \ r </pre>" ""
+        for i in range (len (args)): x += args [i] + "\ n"
+        return f""" <pre {properties} "> \ n {x} \ r </pre>"""
