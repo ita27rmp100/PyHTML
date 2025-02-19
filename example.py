68ed86dfc8e81edb81857f2ec448d3f9849a7365
Abderahim <ita27rmp100@gmail.com>
2025-02-19 18:52:19 +0100

    edit the tag_functions file (use only one class and reduce the number of functions inside it

--- example.py
+++ example.py
@@ -1,12 +1,14 @@
-from tag_functions import html_tag , body_tag , head_tag
-# an excample :
+from tag_functions import html_tag
  
 x = html_tag.DOCTYPE_html()+html_tag.html(
-        head_tag.head(
-            head_tag.title("test")
+        html_tag.MainChild(
+            "head","",
+            html_tag.title("test")
         ),
-        body_tag.body("",
-            body_tag.div("",
+        html_tag.MainChild(
+            "body","",        
+            html_tag.tag(
+                "div","",
                 html_tag.tag("h1","","hello world")
             )+html_tag.comment("""how to write "hello world" in html""")
     ))
--- tag_functions.py
+++ tag_functions.py
@@ -1,48 +1,38 @@
 class html_tag:
-    def DOCTYPE_html ():
+    # Declarate type
+    def DOCTYPE_html():
         return "<! DOCTYPE html> \n"
-    def html (*args):
+    # Declare main parts (html>{head,body})
+    def html(*args):
         x = ""
         for i in args: x += str (i) + "\n"
-        print(x)
         return f"<html> \n {x} \r </html>"
-    def comment (* args):
+    def MainChild(type="",properties = "", *args):
         x = ""
+        for i in range (len (args)): x += args [i] + "\n"
+        return f"""<{type} {properties}> \n {x} \r </{type}> """
+    # General head tags
+    def title(title):
+        return f"""<title> {title} </title>"""
+    def meta(properties = "", *args):
+        x = ""
+        for i in range (len (args)): x += args [i] + "\n"
+        return f""" <meta {properties} "> {x} </meta>"""
+    def link(properties = "", *args):
+        x = ""
+        for i in range (len (args)): x += args [i] + "\n"
+        return f"""<link {properties}"> {x} </link> """
+    def JsCss(type="style",*args):
+        x = ""
+        for i in range (len (args)): x += args [i] + "\n"
+        return f"""<{type}> \n {x} \r </{type}>"""
+    # comments
+    def comment(*args):
+        x = "\n"
         for i in args: x += str (i) + "\n"
         return f"<!-- {x} \r -->"
-    def tag (name, properties = "", * args):
+    # for other tags
+    def tag(name, properties = "", *args):
         x = ""
         for i in range (len (args)): x += args [i] + "\n"
-        return f"""<{name} {properties}> \n {x} \r </ {name}>"""
-class head_tag:
-        def head (* args):
-            x = ""
-            for i in range (len (args)): x += args [i] + "\n"
-            return f"""<head> \n {x} \r </head>"""
-        def title (title):
-            return f"""<title> {title} </title>"""
-        def meta (properties = "", * args):
-            x = ""
-            for i in range (len (args)): x += args [i] + "\n"
-            return f""" <meta {properties} "> {x} </meta>"""
-        def link (properties = "", * args):
-            x = ""
-            for i in range (len (args)): x += args [i] + "\n"
-            return f"""<link {properties}"> {x} </link> """
-        def style (* args):
-            x = ""
-            for i in range (len (args)): x += args [i] + "\n"
-            return f"""<style> \n {x} \r </style>"""
-class body_tag:
-    def body (properties = "", * args):
-        x = ""
-        for i in range (len (args)): x += args [i] + "\n"
-        return f"""<body {properties}"> \n {x} \r </body> """
-    def div (properties = "", * args):
-        x = ""
-        for i in range (len (args)): x += args [i] + "\n"
-        return f"""<div {properties}"> \n {x} \r </div> """
-    def pre (properties = "", * args):
-        x = ""
-        for i in range (len (args)): x += args [i] + "\n"
-        return f""" <pre {properties} "> \n {x} \r </pre>"""
+        return f"""<{name} {properties}> \n {x} \r </{name}>"""
