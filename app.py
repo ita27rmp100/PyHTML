d008a6020fc7fde97ccaea7a271824b0edf31f57
Abderahim <ita27rmp100@gmail.com>
2025-02-20 15:13:04 +0100

    Update done 20/2/2025

--- app.py
+++ app.py
@@ -1,22 +1,19 @@
-from tag_functions import html_tag
-from run import *
-x = html_tag.DOCTYPE_html()+html_tag.html(
-        html_tag.MainChild(
-            "head","",
-            html_tag.title("test")
-        ),
-        html_tag.MainChild(
-            "body","",        
-            html_tag.tag(
-                "div","",
-                html_tag.tag("h1","","hello world")
-            )+html_tag.comment("""how to write "hello world" in html""")
-    ))
-y = html_tag.tag("h1","","hello world")
-Run(x,"x")
-Run(y,"y")
+from oop import HTMLTags as t
+from oop import FileHandling as hf
 
-# with open(r"test.html", mode="w") as file :
-#     file.write(x)
-#     import os
-#     os.startfile(r"test.html")
+tryCode = t.DOCTYPE_html()+t.html(
+        t.MainChild(
+            "head","",
+            t.title("test")
+        ),
+        t.MainChild(
+            "body","",        
+            t.tag(
+                "div","",
+                t.tag("h1","","hello world")
+            )
+    ))
+done = t.tag("h1","","Update Done")
+
+hf.save(done,"saveExample")
+hf.run(tryCode,"runExample")
--- oop.py
+++ oop.py
@@ -1,7 +1,7 @@
-class html_tag:
+class HTMLTags:
     # Declarate type
     def DOCTYPE_html():
-        return "<! DOCTYPE html> \n"
+        return "<!DOCTYPE html> \n"
     # Declare main parts (html>{head,body})
     def html(*args):
         x = ""
@@ -36,3 +36,12 @@ class html_tag:
         x = ""
         for i in range (len (args)): x += args [i] + "\n"
         return f"""<{name} {properties}> \n {x} \r </{name}>"""
+class FileHandling : 
+    def save(content,fileName) :
+        with open(f"./htmls/{fileName}.html", mode="w") as file :
+            file.write(content)
+    def run(content,fileName) :
+        # the function will save then run the html file
+        FileHandling.save(content,fileName)
+        import os
+        os.startfile(os.path.join(os.getcwd(),'htmls',f'{fileName}.html'))
